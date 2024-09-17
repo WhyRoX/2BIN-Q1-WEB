ba3027470f87036222ad35cc755e077f5a500ecc
Enes Yumer <enes.12.yumer@gmail.com>
2024-09-17 15:15:52 +0200

    new: ex1.2 + corr branches

---
+++ exercices/1.1/basic/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+import drinkRouter from "./routes/drinks";
+import filmRouter from "./routes/films";
+
+import { ErrorRequestHandler } from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+app.use("/drinks", drinkRouter);
+app.use("/films", filmRouter);
+
+// Error handler
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+};
+  
+app.use(errorHandler);
+  
+
+export default app;
